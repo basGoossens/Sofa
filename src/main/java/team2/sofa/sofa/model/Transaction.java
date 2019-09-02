e9c5d932545ee6b16c1c4dc5b6f54abb61893b8c
Kirsten <kirsten.jalvingh@hva.nl>
2019-09-02 13:55:32 +0200

    Transcaction en PQD modellen gevuld

--- src/main/java/team2/sofa/sofa/model/Transaction.java
+++ src/main/java/team2/sofa/sofa/model/Transaction.java
@@ -1,4 +1,81 @@
 package team2.sofa.sofa.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private double amount;
+    private String description;
+    private LocalDate date;
+    @ManyToOne
+    private Account creditAccount;
+    @ManyToOne
+    private Account debitAccount;
+
+    public Transaction(double amount, String description, LocalDate date, Account creditAccount, Account debitAccount){
+        this();
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+        this.creditAccount = creditAccount;
+        this.debitAccount = debitAccount;
+    }
+
+    public Transaction(){
+        super();
+        this.id = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Account getCreditAccount() {
+        return creditAccount;
+    }
+
+    public void setCreditAccount(Account creditAccount) {
+        this.creditAccount = creditAccount;
+    }
+
+    public Account getDebitAccount() {
+        return debitAccount;
+    }
+
+    public void setDebitAccount(Account debitAccount) {
+        this.debitAccount = debitAccount;
+    }
 }
