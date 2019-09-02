a857d4ae388df8f0b8bd98c91290c0af3aa32bdd
Danielle van den Bos <danielle@vdbos.nl>
2019-09-02 15:21:43 +0200

    IndexCotroller aangevuld

--- src/main/java/team2/sofa/sofa/controller/IndexController.java
+++ src/main/java/team2/sofa/sofa/controller/IndexController.java
@@ -1,4 +1,27 @@
 package team2.sofa.sofa.controller;
 
+import org.hibernate.dialect.hint.IndexQueryHintHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import team2.sofa.sofa.model.Client;
+import team2.sofa.sofa.model.Employee;
+import team2.sofa.sofa.model.User;
+
+@Controller
 public class IndexController {
+
+    @GetMapping(value = login_client)
+    public String indexHandler(Model model) {
+        User user = new Client();
+        model.addAttribute("member", user);
+        return "login_client";
+    }
+
+    @GetMapping(value = login_employee)
+    public String indexHandler(Model model){
+        User user = new Employee();
+        model.addAttribute("employee", user);
+        return "login_employee";
+    }
 }
--- src/main/java/team2/sofa/sofa/controller/LoginController.java
+++ src/main/java/team2/sofa/sofa/controller/LoginController.java
@@ -1,5 +1,6 @@
 package team2.sofa.sofa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.model.User;
+import team2.sofa.sofa.service.PasswordValidator;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    PasswordValidator validator;
+/*
     @GetMapping(value = "login")
     public String indexHandler(Model model) {
         User user = new Client();
         model.addAttribute("member", user);
-        return "login";
+        return "login";*/
     }
 
     @PostMapping(value = "loginHandler")
