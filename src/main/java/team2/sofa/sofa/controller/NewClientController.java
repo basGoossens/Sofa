b5929972474a948176d05dc335538a0edb5307a3
Aat Niehot <aat.niehot@hva.nl>
2019-09-03 10:54:17 +0200

    new_account.html working Controller working (basics) index.html added link to new_account

--- src/main/java/team2/sofa/sofa/controller/NewClientController.java
+++ src/main/java/team2/sofa/sofa/controller/NewClientController.java
@@ -4,12 +4,23 @@ package team2.sofa.sofa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import team2.sofa.sofa.model.Client;
 
 @Controller
 public class NewClientController {
 
     @GetMapping(value = "new_account")
     public String newClientHandler(Model model){
+    Client client = new Client();
+    model.addAttribute("client", client);
         return "new_account";
     }
-}
+
+
+    @PostMapping(value = "newAccountHandler")
+    public String loginHandler(@ModelAttribute Client client, Model model) {
+        return "login";
+    }
+    }
