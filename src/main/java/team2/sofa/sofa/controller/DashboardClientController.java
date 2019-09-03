2aeab0d000b79d28639b39ab60c3271d39cde977
Danielle van den Bos <danielle@vdbos.nl>
2019-09-03 14:00:46 +0200

    DashboardClientController en DashboardEmployeeController toegevoegd met terug knop.

--- src/main/java/team2/sofa/sofa/controller/DashboardClientController.java
+++ src/main/java/team2/sofa/sofa/controller/DashboardClientController.java
@@ -1,18 +1,18 @@
 package team2.sofa.sofa.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import team2.sofa.sofa.model.Client;
 
-import javax.persistence.Entity;
-
-
-@Entity
+@Controller
 public class DashboardClientController {
 
     //Ge terug naat de overview pagina van Client//
     @GetMapping(value = "client_view")
     public String backToClientView(Model model) {
+        Client client = new Client();
+        model.addAttribute("client", client);
         return "client-view";
 
     }
--- src/main/java/team2/sofa/sofa/controller/DashboardEmployeeController.java
+++ src/main/java/team2/sofa/sofa/controller/DashboardEmployeeController.java
@@ -1,19 +1,19 @@
 package team2.sofa.sofa.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.model.Employee;
 
-import javax.persistence.Entity;
 
-@Entity
+@Controller
 public class DashboardEmployeeController {
-
 
     //Ga terug naar de overview pagina van Employee//
     @GetMapping(value = "employee_view")
     public String backToEmployeeView(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
         return "employee-view";
     }
 }
