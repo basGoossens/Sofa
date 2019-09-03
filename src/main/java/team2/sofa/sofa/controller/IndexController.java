09b0d029c54007753d8a299be58669aae368d225
Danielle van den Bos <danielle@vdbos.nl>
2019-09-03 09:08:42 +0200

    EmployeeDao, indexController en LoginController aangemaakt en aangepast.

--- src/main/java/team2/sofa/sofa/controller/IndexController.java
+++ src/main/java/team2/sofa/sofa/controller/IndexController.java
@@ -1,13 +1,22 @@
 package team2.sofa.sofa.controller;
 
-import org.hibernate.dialect.hint.IndexQueryHintHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import team2.sofa.sofa.model.Client;
-import team2.sofa.sofa.model.Employee;
-import team2.sofa.sofa.model.User;
+import team2.sofa.sofa.model.dao.ClientDao;
+
 
 @Controller
 public class IndexController {
+    @Autowired
+    ClientDao clientDao;
+
+    @GetMapping(value = "login")
+    public String indexHandler(Model model) {
+        Client client = new Client();
+        model.addAttribute("member", client);
+        return "login";
+    }
 }
--- src/main/java/team2/sofa/sofa/controller/LoginController.java
+++ src/main/java/team2/sofa/sofa/controller/LoginController.java
@@ -1,18 +1,18 @@
 package team2.sofa.sofa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import team2.sofa.sofa.model.Client;
-import team2.sofa.sofa.model.Global;
-import team2.sofa.sofa.model.User;
+import team2.sofa.sofa.model.Employee;
 import team2.sofa.sofa.model.dao.ClientDao;
+import team2.sofa.sofa.model.dao.EmployeeDao;
 import team2.sofa.sofa.service.PasswordValidator;
 
-import java.util.List;
 
 @Controller
 public class LoginController {
@@ -20,21 +20,37 @@ public class LoginController {
     @Autowired
     ClientDao clientDao;
 
-    @GetMapping(value = "login")
-    public String indexHandler(Model model) {
-        Client client = new Client();
-        model.addAttribute("member", client);
-        return "login";
+    @Autowired
+    EmployeeDao employeeDao;
+
+
+
+    @GetMapping(value = "login_employee.html")
+    public String goTologinEmployeeHandler(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "login_employee.html";
+
     }
 
-    @PostMapping(value = "loginHandler")
-    public String loginHandler(@ModelAttribute Client client, Model model) {
+    @PostMapping(value = "loginClientHandler")
+    public String loginClientHandler(@ModelAttribute Client client, Model model) {
         PasswordValidator passwordValidator = new PasswordValidator();
         boolean loginOk = passwordValidator.validateClientPassword(client);
         if (loginOk)  {
-            Client currentClient = clientDao.findByUsername(client.getUserName());
+            /*Client currentClient = clientDao.findByUsername(client.getUserName());*/
             return "client_view";
         }
-        else return "login_fail";
+        else return "login";
     }
+    @PostMapping(value = "loginEmployeeHandler")
+    public String loginEmployeeHandler(@ModelAttribute Employee employee, Model model) {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        boolean loginOk = passwordValidator.validateEmployeePassword(employee);
+        if (loginOk) {
+            Employee currentEmployee = employeeDao.findByUsername(employee.getUserName());
+            return "employee_view";
+        } else return "login";
+    }
+
 }
