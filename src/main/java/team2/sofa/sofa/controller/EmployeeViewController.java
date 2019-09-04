f4edb9d61b5ecae1eccc4bfa229e500e3343bc7a
gracejones <rene.van.der.stok@hva.nl>
2019-09-04 16:37:06 +0200

    gewerkt aan link van employee view naar most active clients

--- src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
+++ src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import team2.sofa.sofa.model.Account;
 import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.model.dao.AccountDao;
+import team2.sofa.sofa.model.dao.ClientDao;
 import team2.sofa.sofa.service.TopTenHighestBalanceFinder;
 import team2.sofa.sofa.service.TopTenMostActiveClientFinder;
 
@@ -20,6 +21,9 @@ public class EmployeeViewController {
     @Autowired
     AccountDao accountDao;
 
+    @Autowired
+    ClientDao clientDao;
+
 
     @GetMapping(value = "EmployeeViewHandler")
     public String EmployeeViewHandler(@RequestParam(name = "id") int id, Account account, Model model) {
@@ -29,5 +33,14 @@ public class EmployeeViewController {
         model.addAttribute("account", chosenAccount);
         return "dashboard_employee";
     }
+
+
+//werkt nog niet
+//    @GetMapping(value = "TenMostActiveHandler")
+//    public String TenMostActiveHandler(@RequestParam(name = "id") int id, Client client, Model model) {
+//        Client chosenClient = clientDao.findClientById(id);
+//        model.addAttribute("client", chosenClient);
+//        return "client_view";
+//    }
 
 }
