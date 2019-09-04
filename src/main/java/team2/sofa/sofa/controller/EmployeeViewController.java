212b121604664271ae18434fd3f4febf78de63d5
gracejones <rene.van.der.stok@hva.nl>
2019-09-04 16:25:17 +0200

    klik op IBAN in employee_view gaat door naar dashboard_employee

--- src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
+++ src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import team2.sofa.sofa.model.Account;
 import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.model.dao.AccountDao;
@@ -16,11 +17,17 @@ import java.util.Map;
 @Controller
 public class EmployeeViewController {
 
+    @Autowired
+    AccountDao accountDao;
 
-    @GetMapping(value = "employee_view")
-    public String EmployeeViewHandler(Model model) {
 
-        return "employee_view";
+    @GetMapping(value = "EmployeeViewHandler")
+    public String EmployeeViewHandler(@RequestParam(name = "id") int id, Account account, Model model) {
+        System.out.println("HEY!" + id);
+        System.out.println("HEY" + account);
+        Account chosenAccount = accountDao.findAccountById(id);
+        model.addAttribute("account", chosenAccount);
+        return "dashboard_employee";
     }
 
 }
