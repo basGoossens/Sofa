76502f8ea75d189f5bacffe0347cb46ce1e2e4ae
gracejones <rene.van.der.stok@hva.nl>
2019-09-06 10:13:55 +0200

    Bezig met aparte ClientView voor Employees

--- src/main/java/team2/sofa/sofa/service/TopTenMostActiveClientFinder.java
+++ src/main/java/team2/sofa/sofa/service/TopTenMostActiveClientFinder.java
@@ -17,28 +17,6 @@ public class TopTenMostActiveClientFinder {
     @Autowired
     ClientDao clientDao;
 
-
-//    public Map<Client, Integer> getTopTenMostActiveClients() {
-//        //lijst maken met klanten en hun totaal aantal transacties
-//        LinkedHashMap<Client, Integer> totalNumberTransactionsPerClient = new LinkedHashMap<>();
-//
-//        for (Client client : clientDao.findAll()
-//        ) { totalNumberTransactionsPerClient.put(client, client.getTotalNumberOfTransactions());
-//
-//        }
-//        //de lijst sorteren op aantal transacties en teruggeven
-//        totalNumberTransactionsPerClient.entrySet().stream().sorted(Map.Entry.comparingByValue());
-//
-//        ArrayList<Client> topTenValues = new ArrayList<>();
-//        for (int i = 0; i < 10 ; i++) {
-//            totalNumberTransactionsPerClient.get(i);
-//        }
-//
-//        return totalNumberTransactionsPerClient;
-//
-//        }
-//    }
-
     public List<Client> getTopTenMostActiveClients(){
 
         return clientDao.findTop10ByOrderByTotalNumberOfTransactionsDesc();
