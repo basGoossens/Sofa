25ad1a25189ad0370df39135b1e34ecf34b06cad
Aat Niehot <aat.niehot@hva.nl>
2019-09-10 15:08:19 +0200

    EmployeeViewController opgeschoond Templates/static/css default.css aangemaakt.

--- src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
+++ src/main/java/team2/sofa/sofa/controller/EmployeeViewController.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import team2.sofa.sofa.model.*;
-import team2.sofa.sofa.model.dao.AccountDao;
-import team2.sofa.sofa.model.dao.ClientDao;
-import team2.sofa.sofa.service.TopTenHighestBalanceFinder;
 import team2.sofa.sofa.service.TopTenMostActiveClient;
 
 import java.util.ArrayList;
