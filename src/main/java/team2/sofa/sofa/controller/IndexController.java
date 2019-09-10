50a62fc6fa320d1ec8933c4269260bf34b58e914
Aat Niehot <aat.niehot@hva.nl>
2019-09-10 15:26:28 +0200

    Cleaned indexController

--- src/main/java/team2/sofa/sofa/controller/IndexController.java
+++ src/main/java/team2/sofa/sofa/controller/IndexController.java
@@ -2,13 +2,7 @@ package team2.sofa.sofa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import team2.sofa.sofa.model.Client;
-import team2.sofa.sofa.model.LoginForm;
 import team2.sofa.sofa.model.dao.ClientDao;
 
 
