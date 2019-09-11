e1a2c5461c71c3c00ce48c83f528ce3ace4d6d4c
Joost Kager <joost.kager@hva.nl>
2019-09-11 15:09:36 +0200

    laatste comments en opschoning FundTransfer service

--- src/main/java/team2/sofa/sofa/service/FundTransfer.java
+++ src/main/java/team2/sofa/sofa/service/FundTransfer.java
@@ -3,14 +3,14 @@ package team2.sofa.sofa.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import team2.sofa.sofa.model.*;
+import team2.sofa.sofa.model.Account;
+import team2.sofa.sofa.model.Client;
+import team2.sofa.sofa.model.Transaction;
+import team2.sofa.sofa.model.TransactionForm;
 import team2.sofa.sofa.model.dao.AccountDao;
 import team2.sofa.sofa.model.dao.ClientDao;
-import team2.sofa.sofa.model.dao.PrivateAccountDao;
 import team2.sofa.sofa.model.dao.TransactionDao;
-import team2.sofa.sofa.service.BalanceChecker;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,10 +25,13 @@ public class FundTransfer {
     @Autowired
     ClientDao clientDao;
 
-    public FundTransfer(){super();}
+    public FundTransfer() {
+        super();
+    }
 
     /**
      * wordt gebruikt om form in Money_transfer voor te bereiden
+     *
      * @param id
      * @param model
      * @return
@@ -40,21 +43,42 @@ public class FundTransfer {
         model.addAttribute("transactionForm", t);
         return "money_transfer";
     }
-    public String readyDashboard(TransactionForm transactionForm, Model model){
+
+    /**
+     * helper methode om juiste data in te laden in model voor dashboard na uitvoeren van betaling
+     *
+     * @param transactionForm
+     * @param model
+     * @return
+     */
+    public String readyDashboard(TransactionForm transactionForm, Model model) {
         Account a = accountDao.findAccountByIban(transactionForm.getDebetAccount());
         model.addAttribute("account", a);
         return "dashboard_client";
     }
 
-    public void procesTransaction(TransactionForm transactionForm){
+    /**
+     * verwerkt transactie nadat alles is gecontroleerd op juiste input
+     *
+     * @param transactionForm
+     */
+    public void procesTransaction(TransactionForm transactionForm) {
         double amount = transactionForm.getAmount();
         Account debit = accountDao.findAccountByIban(transactionForm.getDebetAccount());
         Account credit = accountDao.findAccountByIban(transactionForm.getCreditAccount());
         Transaction transaction = new Transaction(amount, transactionForm.getDescription(), credit, debit);
-        storeTransaction(debit,credit,transaction);
+        storeTransaction(debit, credit, transaction);
     }
 
-    private void storeTransaction(Account debit, Account credit, Transaction transaction){
+    /**
+     * helper methode die wordt gebruikt door procesTransaction() om de Transactie daadwerkelijk in DB op te slaan
+     * en de balance in debit en credit rekening aan te passen en te updaten in DB
+     *
+     * @param debit
+     * @param credit
+     * @param transaction
+     */
+    private void storeTransaction(Account debit, Account credit, Transaction transaction) {
         debit.addTransaction(transaction);
         credit.addTransaction(transaction);
         debit.lowerBalance(transaction.getAmount());
@@ -64,15 +88,22 @@ public class FundTransfer {
         accountDao.save(credit);
     }
 
-
-    public boolean checkBalance(TransactionForm transactionForm){
+    /**
+     * methode die vanuit controller wordt aangeroepen voor controle op voldoende saldo op debit rekening
+     *
+     * @param transactionForm afkomstig vanuit controller PaymentController
+     * @return
+     */
+    public boolean checkBalance(TransactionForm transactionForm) {
         double amount = transactionForm.getAmount();
         Account account = accountDao.findAccountByIban(transactionForm.getDebetAccount());
-        return checkBalance(amount,account);
+        return checkBalance(amount, account);
     }
+
     /**
-     * controle of er voldoende saldo is op debitrekening
-     * @param amount bedrag dat is ingevoerd in formulier money_transfer
+     * methode die daadwerkelijk controleert of er voldoende saldo is op debitrekening
+     *
+     * @param amount  bedrag dat is ingevoerd in formulier money_transfer
      * @param account het debitAccount dat gebruikt is in het formulier money_transfer
      * @return
      */
@@ -83,20 +114,22 @@ public class FundTransfer {
 
     /**
      * controle of ingevoerd IBAN creditAccount een bestaand IBAN is
+     *
      * @param creditIban het IBAN dat is ingevoerd in money_transfer form in veld CreditAccount
      * @return
      */
-    public boolean checkToAccount (String creditIban){
+    public boolean checkToAccount(String creditIban) {
         return accountDao.existsAccountByIban(creditIban);
     }
-    public boolean nameCheckIban(String lastName, String creditIban){
+
+    public boolean nameCheckIban(String lastName, String creditIban) {
         Account creditAccount = accountDao.findAccountByIban(creditIban);
         List<Client> owners = creditAccount.getOwners();
-        for (Client owner: owners){
+        for (Client owner : owners) {
             if (owner.getLastName().equals(lastName)) {
                 return true;
             }
         }
-       return false;
+        return false;
     }
 }
