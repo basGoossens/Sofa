d9d84998f91864565814e9d047d5993291283863
Joost Kager <joost.kager@hva.nl>
2019-09-13 23:05:21 +0200

    opschonen voor push

--- src/main/java/team2/sofa/sofa/controller/ClientViewController.java
+++ src/main/java/team2/sofa/sofa/controller/ClientViewController.java
@@ -3,11 +3,8 @@ package team2.sofa.sofa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import team2.sofa.sofa.model.*;
+import team2.sofa.sofa.model.Account;
 import team2.sofa.sofa.service.Clientview;
 
 @Controller
@@ -16,18 +13,8 @@ public class ClientViewController {
     @Autowired
     Clientview clientview;
 
-//    @GetMapping(value = "PrivateAccountListHandler")
-//    public String clientViewHandler(@RequestParam(name = "id") int id,  Model model){
-//        return clientview.accountFinderClient(id, model, false);
-//    }
-//
-//    @GetMapping(value = "BusinessAccountListHandler")
-//    public String BusinessAccountListHandler(@RequestParam(name = "id") int id, Model model){
-//        return clientview.accountFinderClient(id, model, true);
-//    }
-
     @PostMapping(value = "AccountListHandler")
-    public String clientView(Account account, Model model){
+    public String clientView(Account account, Model model) {
         return clientview.accountFinderClient(account.getId(), model);
 
     }
