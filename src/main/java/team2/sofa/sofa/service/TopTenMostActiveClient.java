de94344558997513504400a1037ab65caaa2d31b
gracejones <rene.van.der.stok@hva.nl>
2019-09-18 10:52:04 +0200

    commentaar geschreven bij TopTenMostActiveClient en TopTenHighestBalanceFinder

--- src/main/java/team2/sofa/sofa/service/TopTenMostActiveClient.java
+++ src/main/java/team2/sofa/sofa/service/TopTenMostActiveClient.java
@@ -19,14 +19,28 @@ public class TopTenMostActiveClient {
     @Autowired
     ClientDao clientDao;
 
-    public List<Client> getTopTenMostActiveClients(){
+    /**
+     * methode die de tien meest actieve klanten (hoogste totaal aantal transacties uit de database haalt
+     *
+     * @return een geordende lijst met objecten van het type Client.
+     */
 
+    public List<Client> getTopTenMostActiveClients() {
         return clientDao.findTop10ByOrderByTotalNumberOfTransactionsDesc();
     }
 
+    /**
+     * Deze methode wordt aangeroepen in EmployeeViewController, in de methode TenMostActiveHandler,
+     * zodra er in employee_view_mkb.html op een naam in de lijst van meest actieve klanten wordt geklikt.
+     * Ze zorgt ervoor dat de gegevens van de juiste klant worden geleden,
+     * en dat de lijst met Accounts gesplitst wordt in Private en Business,
+     * wat nodig is voor de functionaliteit van client_view_for_employee.html
+     *
+     * @param id, model, deze worden meegegeven bij het klikken op de naam.
+     *@return client_view_for_employee - een nieuwe pagina wordt geladen adhv de klant die is gekoppeld aan de id.*/
+    
     public String mostActiveClientBuilder(int id, Model model) {
         Client chosenClient = clientDao.findClientById(id);
-        //            Accounts van klant scheiden in business en private
         Login.splitPrivateAndBusiness(chosenClient, model);
         model.addAttribute("client", chosenClient);
         return "client_view_for_employee";
