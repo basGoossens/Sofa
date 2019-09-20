a07791c169b7f7c7bb34b194f0e1679d8e8d941e
Aat Niehot <aat.niehot@hva.nl>
2019-09-20 12:00:47 +0200

    Top menu added new Business account header centered New business account html + controller added

--- src/main/java/team2/sofa/sofa/controller/NewBusinessClientController.java
+++ src/main/java/team2/sofa/sofa/controller/NewBusinessClientController.java
@@ -1,6 +1,5 @@
 package team2.sofa.sofa.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,32 +8,35 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import team2.sofa.sofa.model.Business;
 import team2.sofa.sofa.model.Client;
 import team2.sofa.sofa.service.NewAccountChecker;
 
 import javax.validation.Valid;
 
 @Controller
-public class NewClientController {
+public class NewBusinessClientController {
+
 
     @Autowired
     NewAccountChecker newAccountChecker;
 
-    @GetMapping(value = "new_account")
-    public String newClientHandler(Model model) {
+
+    @GetMapping(value = "new_business")
+    public String newClientBusinessHandler(Model model) {
         Client client = new Client();
         model.addAttribute("client", client);
-        return "new_account";
+        return "new_business";
     }
 
 
-    @PostMapping(value = "newAccountHandler")
-    public String newAccountHandler(@ModelAttribute @Valid Client client, BindingResult result, Model model, Errors error) {
-        if (result.hasErrors()) {
+    @PostMapping(value = "newBusinessAccountHandler")
+    public String newAccountBusinessHandler(@ModelAttribute @Valid Client client, BindingResult result, Model model, Errors error) {
+        if (result.hasErrors()){
             model.addAttribute("error", error);
-            return "new_account";
+            return "new_business";
         }
         return newAccountChecker.processApplication(client, model);
     }
-
 }
+
