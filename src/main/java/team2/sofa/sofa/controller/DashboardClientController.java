9bf75aea46f57dc1421948ce05701160e056f7d9
Joost Kager <joost.kager@hva.nl>
2019-09-20 22:26:13 +0200

    klant kan username en beveiligingscode voor toevoegen nieuwe rekeninghouder van rekening invoeren, dit wordt opgeslagen in DB, nieuwe rekeninghouder ziet knop koppel rekening alleen als er een koppeling mogelijk is.

--- src/main/java/team2/sofa/sofa/controller/DashboardClientController.java
+++ src/main/java/team2/sofa/sofa/controller/DashboardClientController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import team2.sofa.sofa.model.Account;
+import team2.sofa.sofa.model.Connector;
+import team2.sofa.sofa.model.dao.AccountDao;
+import team2.sofa.sofa.model.dao.ConnectorDao;
 import team2.sofa.sofa.service.FundTransfer;
 import team2.sofa.sofa.service.Login;
+
+import java.util.Map;
 
 @Controller
 public class DashboardClientController {
@@ -17,15 +22,31 @@ public class DashboardClientController {
     FundTransfer fundTransfer;
     @Autowired
     Login login;
-
-    @GetMapping(value = "TransferHandler")
-    public String transfer(@RequestParam(name = "id") int id, Model model) {
-        return fundTransfer.readyTransaction(id, model);
-    }
+    @Autowired
+    AccountDao accountDao;
+    @Autowired
+    ConnectorDao connectorDao;
 
     @PostMapping(value = "TransferHandler")
     public String transfer(Account account, Model model) {
         return fundTransfer.readyTransaction(account.getId(), model);
+    }
+
+    @PostMapping(value = "ConnectAccount")
+    public String connectAccounts(@RequestParam int id, Model model){
+        Account account = accountDao.findAccountById(id);
+        model.addAttribute("account", account);
+        return "connect_accounts";
+    }
+    @PostMapping(value = "ConnectAccount/form")
+    public String connectHandeler(@RequestParam Map<String, Object> body, Model model){
+        String iban = body.get("bankaccount").toString();
+        String user = body.get("newuser").toString();
+        String accescode = body.get("accesscode").toString();
+        Connector connector = new Connector(user,accescode,iban);
+        connectorDao.save(connector);
+        model.addAttribute("account", accountDao.findAccountByIban(iban));
+        return "dashboard_client";
     }
 
     @PostMapping(value = "backToOverview")
