8363f70865a62c2ab3336dde66d1d77af30dfcd6
K.M. Jalvingh <kirsten.jalvingh@hva.nl>
2019-09-24 14:18:40 +0200

    payment data gemaakt

---
+++ src/main/java/team2/sofa/sofa/service/PaymentData.java
@@ -0,0 +1,59 @@
+package team2.sofa.sofa.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+public class PaymentData {
+    private BigDecimal amount;
+    private String description;
+    private LocalDateTime dateTime;
+    private String creditAccount;
+    private String debitAccount;
+
+    public PaymentData() {
+        super();
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getCreditAccount() {
+        return creditAccount;
+    }
+
+    public void setCreditAccount(String creditAccount) {
+        this.creditAccount = creditAccount;
+    }
+
+    public String getDebitAccount() {
+        return debitAccount;
+    }
+
+    public void setDebitAccount(String debitAccount) {
+        this.debitAccount = debitAccount;
+    }
+}
