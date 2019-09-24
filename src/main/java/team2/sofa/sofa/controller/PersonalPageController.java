e6ca8bbeedc11f12ae5a9b03be4d2503abce08c2
Joost Kager <joost.kager@hva.nl>
2019-09-24 10:00:09 +0200

    code wat opgeschoond en overzicht.xls bijgewerkt

--- src/main/java/team2/sofa/sofa/controller/PersonalPageController.java
+++ src/main/java/team2/sofa/sofa/controller/PersonalPageController.java
@@ -3,12 +3,11 @@ package team2.sofa.sofa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import team2.sofa.sofa.model.Account;
 import team2.sofa.sofa.model.Address;
 import team2.sofa.sofa.model.Client;
-import team2.sofa.sofa.model.dao.AddressDao;
-import team2.sofa.sofa.model.dao.ClientDao;
 import team2.sofa.sofa.service.UpdateClient;
 
 import java.util.Map;
@@ -18,41 +17,37 @@ public class PersonalPageController {
 
     @Autowired
     UpdateClient updateClient;
-    @Autowired
-    ClientDao clientDao;
-    @Autowired
-    AddressDao addressDao;
 
     @PostMapping(value = "updateHandler")
     public String updateHandler(@RequestParam int id, Model model) {
-        Client client = clientDao.findClientById(id);
+        Client client = updateClient.findClient(id);
         model.addAttribute("client", client);
         return "edit_client_personalpage";
     }
+
     @PostMapping(value = "changeAddressForm")
-    public String changeAddress(@RequestParam int clientId, @RequestParam int addressId, Model model){
-        Address address = addressDao.findById(addressId).get();
+    public String changeAddress(@RequestParam int clientId, @RequestParam int addressId, Model model) {
+        Address address = updateClient.findAddress(addressId);
         model.addAttribute("address", address);
         model.addAttribute("clientId", clientId);
         return "change_address";
     }
+
     @PostMapping(value = "changeAddress")
-    public String processAddress(@RequestParam Map<String,String> input, @RequestParam int clientId, Model model){
-        Client client = clientDao.findClientById(clientId);
+    public String processAddress(@RequestParam Map<String, String> input, @RequestParam int clientId, Model model) {
+        Client client = updateClient.findClient(clientId);
         Address address = client.getAddress();
         client = updateClient.changeAddress(client, address, input);
         model.addAttribute("client", client);
         model.addAttribute("account", new Account());
         return "client_view";
-
     }
 
-
     @PostMapping(value = "updateClient")
-    public String verwerkUpdate (@RequestParam Map<String, Object> input, Model model){
+    public String processUpdate(@RequestParam Map<String, Object> input, Model model) {
         Client client = updateClient.findClient(Integer.valueOf(input.get("id").toString()));
         String newUsername = input.get("username").toString();
-        if (updateClient.usernameExists(newUsername)){
+        if (updateClient.usernameExists(newUsername)) {
             client = updateClient.processChanges(client, input);
             model.addAttribute("username", "uw gebruikersnaam is niet gewijzigd");
             model.addAttribute("client", client);
@@ -64,5 +59,4 @@ public class PersonalPageController {
         model.addAttribute("account", new Account());
         return "client_view";
     }
-
 }
--- src/main/java/team2/sofa/sofa/service/UpdateClient.java
+++ src/main/java/team2/sofa/sofa/service/UpdateClient.java
@@ -2,17 +2,12 @@ package team2.sofa.sofa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import team2.sofa.sofa.model.Address;
 import team2.sofa.sofa.model.Client;
-import team2.sofa.sofa.model.LoginForm;
 import team2.sofa.sofa.model.dao.AccountDao;
 import team2.sofa.sofa.model.dao.AddressDao;
 import team2.sofa.sofa.model.dao.ClientDao;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -27,21 +22,23 @@ public class UpdateClient {
     @Autowired
     NewAccountChecker checker;
 
-    List<String> errorList;
-
     public UpdateClient() {
         super();
     }
 
-    public Client findClient(int id){
+    public Address findAddress(int id) {
+        return addressDao.findById(id).get();
+    }
+
+    public Client findClient(int id) {
         return clientDao.findClientById(id);
     }
 
-    public boolean usernameExists(String newUsername){
+    public boolean usernameExists(String newUsername) {
         return clientDao.existsClientByUsername(newUsername);
-        }
+    }
 
-    public Client processChanges(Client client, Map<String,Object> input){
+    public Client processChanges(Client client, Map<String, Object> input) {
         client.setFirstName(input.get("firstName").toString());
         client.setPrefix(input.get("prefix").toString());
         client.setLastName(input.get("lastName").toString());
@@ -52,10 +49,10 @@ public class UpdateClient {
         return client;
     }
 
-    public Client changeAddress(Client client, Address address, Map<String,String> input){
+    public Client changeAddress(Client client, Address address, Map<String, String> input) {
         int count = clientDao.countClientsByAddressId(address.getId());
-        if (count > 1){
-            Address changed = new Address(0,input.get("street"), Integer.valueOf(input.get("houseNumber")),input.get("zipCode"),input.get("city"));
+        if (count > 1) {
+            Address changed = new Address(0, input.get("street"), Integer.valueOf(input.get("houseNumber")), input.get("zipCode"), input.get("city"));
             if (checker.AddressExistsChecker(changed)) {
                 Address exists = checker.AddressExists(changed);
                 client.setAddress(exists);
@@ -81,86 +78,4 @@ public class UpdateClient {
         addressDao.save(address);
         return client;
     }
-
-    public List<String> processUpdate(Client client, Map<String, Object> body){
-        if (body.get("firstName").toString() != "" && !body.get("firstName").equals(client.getFirstName()))
-            client.setFirstName(body.get("firstName").toString());
-        if (body.get("prefix").toString() != "" && !body.get("prefix").equals(client.getPrefix()))
-            client.setPrefix(body.get("prefix").toString());
-        if (body.get("lastName").toString() != "" && !body.get("lastName").equals(client.getLastName()))
-            client.setLastName(body.get("lastName").toString());
-        if (body.get("zipCode").toString() != "" && body.get("houseNumber").toString() != "")
-            if (!body.get("zipCode").equals(client.getAddress().getZipCode()) || !body.get("houseNumber").equals(client.getAddress().getHouseNumber())){
-                checkDeleteAddress(client.getAddress()); //method checks and deletes if no other occupants on address
-                int addNr = Integer.parseInt(body.get("zipCode").toString());
-                String addStreet = body.get("street").toString();
-                String addZip = body.get("zipCode").toString();
-                String addCity = body.get("city").toString();
-                if (addressDao.findAddressByZipCodeAndHouseNumber(addZip, addNr) != null) {
-                    Address newAdd = new Address(0, addStreet, addNr, addZip, addCity);
-                    addressDao.save(newAdd);
-                    client.setAddress(newAdd);
-                } else client.setAddress(addressDao.findAddressByZipCodeAndHouseNumber(addZip, addNr));
-            }
-        if (body.get("ssn").toString() != "" && !body.get("ssn").equals(client.getSsn()))
-            if (checkSSN(body.get("ssn").toString())) client.setSsn(body.get("ssn").toString());
-        if (body.get("email").toString() != "" && !body.get("email").equals(client.getEmail()))
-            client.setEmail(body.get("email").toString());
-        if (body.get("telephoneNr").toString() != "" && !body.get("telephoneNr").equals(client.getTelephoneNr()))
-            client.setTelephoneNr(body.get("telephoneNr").toString());
-        if (body.get("birthday").toString() != "" && !body.get("birthday").equals(client.getBirthday()))
-            client.setBirthday(body.get("birthday").toString());
-        if (body.get("gender").toString() != "" && !body.get("gender").equals(client.getBirthday()))
-            client.setGender(body.get("gender").toString());
-        if (body.get("username").toString() != "" && !body.get("username").equals(client.getUsername()))
-            client.setUsername(body.get("username").toString());
-        if (body.get("password").toString() != "" && !body.get("password").equals(client.getPassword()))
-            client.setPassword(body.get("password").toString());
-        return errorList;
-    }
-
-    public boolean checkSSN(String ssn) {
-        if (clientDao.findClientBySsn(ssn) != null) {
-            errorList.add("Gebruiker met dit BSN bestaat al.");
-            return false;
-        }
-        if (ssn.length() != 9) {
-            errorList.add("BSN heeft geen 9 cijfers.");
-            return false;
-        }
-        if (!SSNFunctionality.ssnCheck(ssn)) {
-            errorList.add("Geen geldig BSN ingevoerd.");
-            return false;
-        }
-        return true;
-    }
-
-    public void checkDeleteAddress (Address adres){
-        Address currentAddress = addressDao.findAddressByZipCodeAndHouseNumber(adres.getZipCode(), adres.getHouseNumber());
-        List<Client> allOccupants = clientDao.findClientsByAddressId(currentAddress.getId());
-        if (allOccupants.size() < 2) addressDao.delete(adres);
-    }
-
-    public boolean checkPassword(String password){
-        if (password.length() < 8) {
-            errorList.add("Geen geldig wachtwoord. Onvoldoende lengte.");
-            return false;
-        }
-        int countSymbols = 0;
-        int countInts = 0;
-        int countLetters = 0;
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-            if (c >= '0' && c <= '9') countInts ++;
-            if (c >= 'A' && c <= 'Z') countLetters ++;
-            if (c >= 'a' && c <= 'z') countLetters ++;
-            else countSymbols ++;
-        }
-        if (countSymbols > 0 && countInts > 0 && countLetters > 0) return true;
-        else {
-            errorList.add("Geen geldig wachtwoord. Een geldig wachtwoord bestaat uit cijfers, letters en symbolen.");
-            return false;
-        }
-    }
-
 }
