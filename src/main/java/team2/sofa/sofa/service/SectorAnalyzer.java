9343f3581cd70fb6f788f21503001432c87735d4
K.M. Jalvingh <kirsten.jalvingh@hva.nl>
2019-09-26 00:55:15 +0200

    SectorAnalyzer businessector lowercase

--- src/main/java/team2/sofa/sofa/service/SectorAnalyzer.java
+++ src/main/java/team2/sofa/sofa/service/SectorAnalyzer.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team2.sofa.sofa.model.dao.AccountDao;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +21,7 @@ public class SectorAnalyzer {
         List<BalancePerSectorData> balancePerSectorList = new ArrayList<>();
         for (String item : balancePerSectorStrings){
             String[]parts = item.split(",");
-            BalancePerSectorData balancePerSector = new BalancePerSectorData(parts[0],Double.valueOf(parts[1]));
+            BalancePerSectorData balancePerSector = new BalancePerSectorData(parts[0].toLowerCase(),Double.valueOf(parts[1]));
             balancePerSectorList.add(balancePerSector);
         }
         return  balancePerSectorList;
