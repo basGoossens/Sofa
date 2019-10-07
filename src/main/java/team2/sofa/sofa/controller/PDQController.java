c895c32e76f1b5beaca8309905427ac7cca6cbeb
K.M. Jalvingh <kirsten.jalvingh@hva.nl>
2019-10-07 11:06:56 +0200

    tussendoor voor de zekerheid

--- src/main/java/team2/sofa/sofa/controller/PDQController.java
+++ src/main/java/team2/sofa/sofa/controller/PDQController.java
@@ -32,7 +32,13 @@ public class PDQController {
     @PostMapping("/paymentmachine/payment/")
     public String TransactionPostController(@RequestBody PaymentData paymentData){
         String returnJson;
-        Account creditAccount = accountDao.findAccountByIban(paymentData.getCreditAccount());
+        try {
+            serializationService.doRestTransaction(paymentData);
+            returnJson ="Approved";
+        } catch (Exception e){
+            returnJson = "Failed" + e.getMessage();
+
+/*        Account creditAccount = accountDao.findAccountByIban(paymentData.getCreditAccount());
         Account debitAccount = accountDao.findAccountByIban(paymentData.getDebitAccount());
         if (creditAccount == null || debitAccount == null){
             returnJson = "Failed";
@@ -47,24 +53,18 @@ public class PDQController {
             } catch (Exception e) {
                 System.out.println(e);
                 returnJson = "Failed";
-            }
+            }*/
         }
         return returnJson;
+        // make a function in service that returns void, but raises exceptions (multiple options) and catch them in controller
     }
 
     @PostMapping("/paymentmachine/coupling/")
     public String CouplingPostController(@RequestBody PaymentMachineConnectionData paymentMachineConnectionData){
         String returnJson = "";
-        //String deserializedString = serializationService.deserializePaymentMachineConnectionData();
         System.out.println(paymentMachineConnectionData.getAccount() + " " + paymentMachineConnectionData.getFiveDigitCode());
-        //String fiveDigitcode = (String) string.subSequence(0,5);
-        //String clientIban = (String) string.subSequence(5, 17);
         try {
             Pdq pdq = pdqDao.findPdqByFiveDigitcode(paymentMachineConnectionData.getFiveDigitCode());
-            System.out.println(paymentMachineConnectionData.getFiveDigitCode());
-            System.out.println(pdq.getFiveDigitcode());
-            System.out.println(paymentMachineConnectionData.getAccount());
-            System.out.println(pdq.getCoupledAccount().getIban());
             if (pdq.getFiveDigitcode().equals(paymentMachineConnectionData.getFiveDigitCode()) ||
                     pdq.getCoupledAccount().getIban().equals(paymentMachineConnectionData.getAccount())){
                 returnJson = pdq.getEightDigitcode();
--- src/main/java/team2/sofa/sofa/service/SerializationService.java
+++ src/main/java/team2/sofa/sofa/service/SerializationService.java
@@ -1,14 +1,31 @@
 package team2.sofa.sofa.service;
 
-import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import team2.sofa.sofa.model.Account;
 import team2.sofa.sofa.model.PaymentData;
-import team2.sofa.sofa.model.PaymentMachineConnectionData;
+import team2.sofa.sofa.model.Transaction;
+import team2.sofa.sofa.model.dao.AccountDao;
+import team2.sofa.sofa.model.dao.BusinessAccountDao;
+import team2.sofa.sofa.model.dao.PdqDao;
 
 @Service
 public class SerializationService {
 
-    public PaymentData deserializePaymentData(String json) {
+
+    @Autowired
+    AccountDao accountDao;
+
+    @Autowired
+    BusinessAccountDao businessAccountDao;
+
+    @Autowired
+    PdqDao pdqDao;
+
+    @Autowired
+    FundTransfer fundTransfer;
+
+/*    public PaymentData deserializePaymentData(String json) {
         Gson gson = new Gson();
         PaymentData paymentData = gson.fromJson(json, PaymentData.class);
         return paymentData;
@@ -18,5 +35,24 @@ public class SerializationService {
         Gson gson = new Gson();
         PaymentMachineConnectionData pmcd = gson.fromJson(json, PaymentMachineConnectionData.class);
         return pmcd;
+    }*/
+
+    public void doRestTransaction(PaymentData paymentData) throws Exception {
+        Account creditAccount = accountDao.findAccountByIban(paymentData.getCreditAccount());
+        Account debitAccount = accountDao.findAccountByIban(paymentData.getDebitAccount());
+        if (creditAccount == null || debitAccount == null) {
+            throw new Exception("ongeldig rekeningnummer");
+        } else if (fundTransfer.insufficientBalance(paymentData.getAmount(), debitAccount)) {
+            throw new Exception("balans onvoldoende");
+        } else {
+            try {
+                Transaction transaction = new Transaction(paymentData.getAmount(), paymentData.getDescription(),
+                        String.valueOf(paymentData.getDate()), creditAccount, debitAccount);
+                fundTransfer.storeTransaction(debitAccount, creditAccount, transaction);
+            } catch (Exception e) {
+                throw new Exception("intern probleem bij de bank");
+            }
+        }
     }
+
 }
